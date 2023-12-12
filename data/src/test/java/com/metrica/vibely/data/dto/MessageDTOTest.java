80cec1e644c74ae3b95c2d74d33c3fc90b281ddc
gonzalorg879 <gonzalorg8799@gmail.com>
2023-12-12 19:44:30 +0100

    Created the new MessageDTOTest

---
+++ data/src/test/java/com/metrica/vibely/data/dto/MessageDTOTest.java
@@ -0,0 +1,23 @@
+package com.metrica.vibely.data.dto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import com.metrica.vibely.data.model.dto.MessageDTO;
+import com.metrica.vibely.data.model.enumerator.MessageState;
+import com.metrica.vibely.data.model.enumerator.MessageStatus;
+
+class MessageDTOTest {
+
+    // <<-CONSTANTS->>
+    private static final String CONTENT = "content";
+    private static final MessageStatus STATUS = MessageStatus.PENDING;
+    private static final MessageState STATE = MessageState.ENABLED;
+    private static final LocalDateTime CREATION_TIMESTAMP = LocalDateTime.now();
+}

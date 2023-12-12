1bd04cf9f18ec9698f8a7ae9b89d020de9c8aaca
gonzalorg879 <gonzalorg8799@gmail.com>
2023-12-12 19:23:48 +0100

    created the userDTOTest

---
+++ data/src/test/java/com/metrica/vibely/data/dto/UserDTOTest.java
@@ -0,0 +1,44 @@
+package com.metrica.vibely.data.dto;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import com.metrica.vibely.data.model.dto.UserDTO;
+import com.metrica.vibely.data.model.enumerator.PrivacyType;
+import com.metrica.vibely.data.model.enumerator.UserState;
+import com.metrica.vibely.data.model.enumerator.UserStatus;
+
+/**
+ * <h1>User DTO Test</h1>
+ *
+ * @since 2023-11-20
+ * @version 1.0
+ * @author Adri
+ */
+class UserDTOTest {
+
+    // <<-CONSTANTS->>
+    private static final String USERNAME = "jdoe";
+    private static final String PASSWORD = "12345";
+    private static final String NICKNAME = "John Doe";
+    private static final String EMAIL = "johndoe@email.com";
+    private static final String APIKEY = "randomApikey";
+    private static final UserState STATE = UserState.ENABLED;
+    private static final PrivacyType PRIVACY = PrivacyType.PUBLIC;
+    private static final UserStatus STATUS = UserStatus.ONLINE;
+    private static final Integer LOGINS = 0;
+    private static final LocalDateTime LAST_CONN_DATE = LocalDateTime.now();
+    private static final LocalDate BLOCKED_DATE = LocalDate.now();
+}

fb03000dd20260abc27b2a833e1785cc2f2b14ec
gonzalorg879 <gonzalorg8799@gmail.com>
2023-12-12 19:42:16 +0100

    Created more tests for ChatDTO

--- data/src/test/java/com/metrica/vibely/data/dto/ChatDTOTest.java
+++ data/src/test/java/com/metrica/vibely/data/dto/ChatDTOTest.java
@@ -27,4 +27,140 @@ class ChatDTOTest {
     private static final ChatType TYPE = ChatType.DIRECT_MESSAGE;
     private static final ChatStatus STATUS = ChatStatus.ACTIVE;
     private static final ChatState STATE = ChatState.ENABLED;
+
+    // <<-METHODS->>
+    @Test
+    @Tag("Constructors")
+    void voidConstructorTest() {
+        UUID chatId = UUID.randomUUID();
+        Set<UUID> participants = new HashSet<>();
+        Set<UUID> messages     = new HashSet<>();
+
+        ChatDTO chat = new ChatDTO();
+
+        chat.setChatId      (chatId);
+        chat.setCreationDate(CREATION_DATE);
+        chat.setType        (TYPE);
+        chat.setStatus      (STATUS);
+        chat.setState		(STATE);
+        chat.setTitle       (TITLE);
+        chat.setLastActivity(LAST_ACTIVITY);
+        chat.setParticipants(participants);
+        chat.setMessages    (messages);
+
+        assertEquals(chatId,        chat.getChatId());
+        assertEquals(CREATION_DATE, chat.getCreationDate());
+        assertEquals(TYPE,          chat.getType());
+        assertEquals(STATUS,        chat.getStatus());
+        assertEquals(STATE,			chat.getState());
+        assertEquals(TITLE,         chat.getTitle());
+        assertEquals(LAST_ACTIVITY, chat.getLastActivity());
+        assertEquals(participants,  chat.getParticipants());
+        assertEquals(messages,      chat.getMessages());
+    }
+
+    @Test
+    @Tag("Constructors")
+    void fullArgsConstructorTest() {
+        UUID chatId = UUID.randomUUID();
+        Set<UUID> participants = new HashSet<>();
+        Set<UUID> messages     = new HashSet<>();
+
+        ChatDTO chat = new ChatDTO(
+                chatId,
+                CREATION_DATE,
+                TYPE,
+                STATUS,
+                STATE,
+                TITLE,
+                LAST_ACTIVITY,
+                participants,
+                messages);
+
+        assertEquals(chatId,        chat.getChatId());
+        assertEquals(CREATION_DATE, chat.getCreationDate());
+        assertEquals(TYPE,          chat.getType());
+        assertEquals(STATUS,        chat.getStatus());
+        assertEquals(STATE, 		chat.getState());
+        assertEquals(TITLE,         chat.getTitle());
+        assertEquals(LAST_ACTIVITY, chat.getLastActivity());
+        assertEquals(participants,  chat.getParticipants());
+        assertEquals(messages,      chat.getMessages());
+    }
+
+    @Test
+    @Tag("Equality")
+    void basicEqualityTest() {
+        ChatDTO chat = new ChatDTO();
+
+        assertEquals   (chat, chat);
+        assertNotEquals(chat, null);
+        assertNotEquals(chat, "");
+    }
+
+    @Test
+    @Tag("Equality")
+    void equalityByIdAndHashCodeTest() {
+        ChatDTO chat1 = new ChatDTO();
+        ChatDTO chat2 = new ChatDTO();
+        UUID uuid = UUID.randomUUID();
+
+        // Both the same ID
+        chat1.setChatId(uuid);
+        chat1.setCreationDate(CREATION_DATE);
+        chat1.setType        (TYPE);
+        chat1.setStatus      (STATUS);
+        chat1.setState		 (STATE);
+        chat1.setTitle       (TITLE);
+        chat1.setLastActivity(LAST_ACTIVITY);
+
+        chat2.setChatId(uuid);
+        chat2.setCreationDate(CREATION_DATE);
+        chat2.setType        (TYPE);
+        chat2.setStatus      (STATUS);
+        chat2.setState		 (STATE);
+        chat2.setTitle       (TITLE);
+        chat2.setLastActivity(LAST_ACTIVITY);
+
+        assertEquals(chat1, chat2);
+        assertEquals(chat1.hashCode(), chat2.hashCode());
+    }
+
+    @Test
+    @Tag("Equality")
+    void inequalityByFieldsTest() {
+        ChatDTO chat1 = new ChatDTO();
+        ChatDTO chat2 = new ChatDTO();
+
+        // Not the same ID
+        chat1.setChatId(UUID.randomUUID());
+        chat1.setCreationDate(CREATION_DATE);
+        chat1.setType        (TYPE);
+        chat1.setStatus      (STATUS);
+        chat1.setState		 (STATE);
+        chat1.setTitle       (TITLE);
+        chat1.setLastActivity(LAST_ACTIVITY);
+
+        chat2.setChatId(UUID.randomUUID());
+        chat2.setCreationDate(CREATION_DATE);
+        chat2.setType        (TYPE);
+        chat2.setStatus      (STATUS);
+        chat2.setState		 (STATE);
+        chat2.setTitle       (TITLE);
+        chat2.setLastActivity(LAST_ACTIVITY);
+
+        assertNotEquals(chat1, chat2);
+    }
+
+    @Test
+    @Tag("Participant")
+    void addParticipantTest() {
+        ChatDTO chat = new ChatDTO();
+        UUID participant = UUID.randomUUID();
+        chat.setParticipants(new HashSet<>());
+
+        assertTrue(chat.addParticipant(participant));
+
+    }
+
 }

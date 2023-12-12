0d87f6f1ccf5c20748e1db14fc52b8503a206821
gonzalorg879 <gonzalorg8799@gmail.com>
2023-12-12 19:38:49 +0100

    Created more tests for PostDTO

--- data/src/test/java/com/metrica/vibely/data/dto/PostDTOTest.java
+++ data/src/test/java/com/metrica/vibely/data/dto/PostDTOTest.java
@@ -27,4 +27,133 @@ class PostDTOTest {
     private static final PostVisibility VISIBILITY = PostVisibility.PUBLIC;
     private static final String CONTENT = "content";
     private static final Integer ZERO = 0;
+    // <<-METHODS->>
+    @Test
+    @Tag("Constructors")
+    void voidConstructorTest() {
+        UUID postId = UUID.randomUUID();
+        UUID owner  = UUID.randomUUID();
+        Set<UUID> comments = new HashSet<>();
+        Set<UUID> likedBy  = new HashSet<>();
+        Set<UUID> savedBy  = new HashSet<>();
+
+        PostDTO post = new PostDTO();
+
+        post.setPostId    (postId);
+        post.setOwner     (owner);
+        post.setPostDate  (POST_DATE);
+        post.setStatus    (STATUS);
+        post.setVisibility(VISIBILITY);
+        post.setContent   (CONTENT);
+        post.setLikes     (ZERO);
+        post.setTimesSaved(ZERO);
+        post.setComments  (comments);
+        post.setLikedBy   (likedBy);
+        post.setSavedBy   (savedBy);
+
+        assertEquals(postId,    post.getPostId());
+        assertEquals(owner,     post.getOwner());
+        assertEquals(POST_DATE, post.getPostDate());
+        assertEquals(STATUS,    post.getStatus());
+        assertEquals(VISIBILITY, post.getVisibility());
+        assertEquals(CONTENT,   post.getContent());
+        assertEquals(ZERO,      post.getLikes());
+        assertEquals(ZERO,      post.getTimesSaved());
+        assertEquals(comments,  post.getComments());
+        assertEquals(likedBy,   post.getLikedBy());
+        assertEquals(savedBy,   post.getSavedBy());
+    }
+
+    @Test
+    @Tag("Constructors")
+    void fullArgsConstructorTest() {
+        UUID postId = UUID.randomUUID();
+        UUID owner  = UUID.randomUUID();
+        Set<UUID> comments = new HashSet<>();
+        Set<UUID> likedBy  = new HashSet<>();
+        Set<UUID> savedBy  = new HashSet<>();
+
+        PostDTO post = new PostDTO(
+                postId,
+                POST_DATE,
+                STATUS,
+                VISIBILITY,
+                CONTENT,
+                ZERO,
+                ZERO,
+                owner,
+                comments,
+                likedBy,
+                savedBy);
+
+        assertEquals(postId,    post.getPostId());
+        assertEquals(owner,     post.getOwner());
+        assertEquals(POST_DATE, post.getPostDate());
+        assertEquals(STATUS,    post.getStatus());
+        assertEquals(VISIBILITY, post.getVisibility());
+        assertEquals(CONTENT,   post.getContent());
+        assertEquals(ZERO,      post.getLikes());
+        assertEquals(ZERO,      post.getTimesSaved());
+        assertEquals(comments,  post.getComments());
+        assertEquals(likedBy,   post.getLikedBy());
+        assertEquals(savedBy,   post.getSavedBy());
+    }
+
+    @Test
+    @Tag("Default values")
+    void notNullableFieldsAndDefaultValuesTest() {
+        Post post = new Post();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDateTime now = LocalDateTime.now();
+
+        post.setPostId    (null);
+        post.setPostDate  (null);
+        post.setStatus    (null);
+        post.setVisibility(null);
+        post.setContent   (null);
+        post.setLikes     (null);
+        post.setTimesSaved(null);
+
+        assertNotNull(post.getPostId());
+        assertEquals(now.format(formatter), post.getPostDate().format(formatter));
+        assertEquals(PostStatus.ACTIVE, post.getStatus());
+        assertEquals(PostVisibility.PUBLIC, post.getVisibility());
+        assertEquals("", post.getContent());
+        assertEquals(0, post.getLikes());
+        assertEquals(0, post.getTimesSaved());
+    }
+
+    @Test
+    @Tag("Equality")
+    void basicEqualityTest() {
+        PostDTO post = new PostDTO();
+
+        assertEquals   (post, post);
+        assertNotEquals(post, null);
+        assertNotEquals(post, "");
+    }
+
+    @Test
+    @Tag("Equality")
+    void equalityByIdAndHashCodeTest() {
+        PostDTO post1 = new PostDTO();
+        PostDTO post2 = new PostDTO();
+        UUID uuid = UUID.randomUUID();
+
+        // Both the same ID
+        post1.setPostId(uuid);
+        post1.setPostDate(POST_DATE);
+        post1.setContent(CONTENT);
+        post1.setStatus(STATUS);
+        post1.setVisibility(VISIBILITY);
+
+        post2.setPostId(uuid);
+        post2.setPostDate(POST_DATE);
+        post2.setContent(CONTENT);
+        post2.setStatus(STATUS);
+        post2.setVisibility(VISIBILITY);
+
+        assertEquals(post1, post2);
+        assertEquals(post1.hashCode(), post2.hashCode());
+    }
 }

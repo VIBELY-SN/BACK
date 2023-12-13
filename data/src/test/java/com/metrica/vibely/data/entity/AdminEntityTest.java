478564b849a909ee445f20b6da50ea4ae9cc422b
Raul <Raul@DESKTOP-T5U920N>
2023-12-13 16:42:51 +0100

    adding constants

--- data/src/test/java/com/metrica/vibely/data/entity/AdminEntityTest.java
+++ data/src/test/java/com/metrica/vibely/data/entity/AdminEntityTest.java
@@ -1,5 +1,19 @@
 package com.metrica.vibely.data.entity;
 
-public class Admin {
+public class AdminEntityTest {
 
+	 // <<-CONSTANTS->>
+    private static final String USERNAME = "jdoe";
+    private static final String PASSWORD = "12345";
+    private static final String NICKNAME = "John Doe";
+    private static final String EMAIL    = "johndoe@email.com";
+    private static final String APIKEY   = "randomApikey";
+    private static final UserState   STATE   = UserState.ENABLED;
+    private static final PrivacyType PRIVACY = PrivacyType.PUBLIC;
+    private static final UserStatus  STATUS  = UserStatus.ONLINE;
+    private static final Integer LOGINS = 0;
+    private static final LocalDateTime LAST_CONN_DATE = LocalDateTime.now();
+    private static final LocalDate     BLOCKED_DATE   = LocalDate.now();
+    
+	
 }

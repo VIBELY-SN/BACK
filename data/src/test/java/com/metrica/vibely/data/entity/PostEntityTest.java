e69ca5207c9612498d0a32f63ad39d52e390bf24
Raul <Raul@DESKTOP-T5U920N>
2023-12-13 16:47:37 +0100

    adding constants

--- data/src/test/java/com/metrica/vibely/data/entity/PostEntityTest.java
+++ data/src/test/java/com/metrica/vibely/data/entity/PostEntityTest.java
@@ -2,4 +2,11 @@ package com.metrica.vibely.data.entity;
 
 public class PostEntityTest {
 
+	// <<-CONSTANTS->>
+    private static final LocalDateTime POST_DATE = LocalDateTime.now();
+    private static final PostStatus STATUS = PostStatus.ACTIVE;
+    private static final PostVisibility VISIBILITY = PostVisibility.PUBLIC;
+    private static final String CONTENT = "content";
+    private static final Integer ZERO = 0;
+	
 }

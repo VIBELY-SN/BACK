c9a7a05d0457e69a855ff667b65bd1cf7b89b43c
Raul <Raul@DESKTOP-T5U920N>
2023-12-13 16:47:46 +0100

    adding constants

--- data/src/test/java/com/metrica/vibely/data/entity/ChatEntityTest.java
+++ data/src/test/java/com/metrica/vibely/data/entity/ChatEntityTest.java
@@ -2,4 +2,13 @@ package com.metrica.vibely.data.entity;
 
 public class ChatEntityTest {
 
+	// <<-CONSTANTS->>
+    private static final String TITLE = "Title";
+    private static final LocalDateTime CREATION_DATE = LocalDateTime.now();
+    private static final LocalDateTime LAST_ACTIVITY = LocalDateTime.now();
+    private static final ChatType   TYPE   = ChatType.DIRECT_MESSAGE;
+    private static final ChatStatus STATUS = ChatStatus.ACTIVE;
+    private static final ChatState  STATE  = ChatState.ENABLED;
+    
+	
 }

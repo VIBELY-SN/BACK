50615264b40436bfe52b712c120e31bf1b906d6e
Raul <Raul@DESKTOP-T5U920N>
2023-12-13 16:47:41 +0100

    adding constants

--- data/src/test/java/com/metrica/vibely/data/entity/MessageEntityTest.java
+++ data/src/test/java/com/metrica/vibely/data/entity/MessageEntityTest.java
@@ -2,4 +2,11 @@ package com.metrica.vibely.data.entity;
 
 public class MessageEntityTest {
 
+	// <<-CONSTANTS->>
+    private static final String CONTENT = "content";
+    private static final MessageStatus STATUS = MessageStatus.PENDING;
+    private static final MessageState STATE = MessageState.ENABLED;
+    private static final LocalDateTime CREATION_TIMESTAMP = LocalDateTime.now();
+
+	
 }

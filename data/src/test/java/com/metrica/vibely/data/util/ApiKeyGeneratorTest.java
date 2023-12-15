3fcdc6f2f1a10b16f1c7e319ece274dc523559e8
gonzalorg879 <gonzalorg8799@gmail.com>
2023-12-15 09:31:08 +0100

    Created the new ApiKeyGeneratorTest

---
+++ data/src/test/java/com/metrica/vibely/data/util/ApiKeyGeneratorTest.java
@@ -0,0 +1,18 @@
+package com.metrica.vibely.data.util;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApiKeyGeneratorTest {
+
+    // <<-CONSTANT->>
+    private static final UUID USER_ID = UUID.fromString("");
+}

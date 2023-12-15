39cc9819876bff1117e03d78b327bed1549c5ec3
gonzalorg879 <gonzalorg8799@gmail.com>
2023-12-15 09:27:33 +0100

    Created more tests for PasswordHasher

--- data/src/test/java/com/metrica/vibely/data/util/PasswordHasherTest.java
+++ data/src/test/java/com/metrica/vibely/data/util/PasswordHasherTest.java
@@ -1,111 +1,38 @@
 package com.metrica.vibely.data.util;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
-
-import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
-import com.metrica.vibely.data.model.dto.MessageDTO;
-import com.metrica.vibely.data.model.enumerator.MessageState;
-import com.metrica.vibely.data.model.enumerator.MessageStatus;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class MessageDTOTest {
+public class PasswordHasherTest {
 
-    // <<-CONSTANTS->>
-    private static final String CONTENT = "content";
-    private static final MessageStatus STATUS = MessageStatus.PENDING;
-    private static final MessageState STATE = MessageState.ENABLED;
-    private static final LocalDateTime CREATION_TIMESTAMP = LocalDateTime.now();
+    // <<-CONSTANT->>
+    private static final String PASSWORD = "password";
 
-    // <<-METHODS->>
     @Test
-    @Tag("Constructors")
-    void voidConstructorTest() {
-        UUID messageId = UUID.randomUUID();
-        UUID chat = UUID.randomUUID();
-        UUID sender = UUID.randomUUID();
+    void differentHashesTest() {
+        String hashedPassword1 = PasswordHasher.hash(PASSWORD);
+        String hashedPassword2 = PasswordHasher.hash(PASSWORD);
 
-        MessageDTO message = new MessageDTO();
-
-        message.setMessageId        (messageId);
-        message.setCreationTimestamp(CREATION_TIMESTAMP);
-        message.setStatus           (STATUS);
-        message.setState			(STATE);
-        message.setContent          (CONTENT);
-        message.setChat             (chat);
-        message.setSender           (sender);
-
-        assertEquals(messageId,          message.getMessageId());
-        assertEquals(CREATION_TIMESTAMP, message.getCreationTimestamp());
-        assertEquals(STATUS,             message.getStatus());
-        assertEquals(STATE,				 message.getState());
-        assertEquals(CONTENT,            message.getContent());
-        assertEquals(chat,               message.getChat());
-        assertEquals(sender,             message.getSender());
+        assertNotEquals(hashedPassword1, hashedPassword2);
     }
 
     @Test
-    @Tag("Constructors")
-    void fullArgsConstructorTest() {
-        UUID messageId = UUID.randomUUID();
-        UUID chat = UUID.randomUUID();
-        UUID sender = UUID.randomUUID();
+    void matchesTest() {
+        String hashedPassword1 = PasswordHasher.hash(PASSWORD);
+        String hashedPassword2 = PasswordHasher.hash(PASSWORD);
 
-        MessageDTO message = new MessageDTO(
-                messageId,
-                CREATION_TIMESTAMP,
-                STATUS,
-                STATE,
-                CONTENT,
-                chat,
-                sender);
-
-        assertEquals(messageId,          message.getMessageId());
-        assertEquals(CREATION_TIMESTAMP, message.getCreationTimestamp());
-        assertEquals(STATUS,             message.getStatus());
-        assertEquals(STATE,				 message.getState());
-        assertEquals(CONTENT,            message.getContent());
-        assertEquals(chat,               message.getChat());
-        assertEquals(sender,             message.getSender());
+        assertTrue(PasswordHasher.matches(PASSWORD, hashedPassword1));
+        assertTrue(PasswordHasher.matches(PASSWORD, hashedPassword2));
     }
 
-    @Test
-    @Tag("Equality")
-    void basicEqualityTest() {
-        MessageDTO message = new MessageDTO();
+    @RepeatedTest(5)
+    void oneWayHashingTest() {
+        String firstTime  = PasswordHasher.hash(PASSWORD);
+        String secondTime = PasswordHasher.hash(firstTime);
 
-        assertEquals   (message, message);
-        assertNotEquals(message, null);
-        assertNotEquals(message, "");
+        assertNotEquals(PASSWORD, secondTime);
     }
-
-    @Test
-    @Tag("Equality")
-    void equalityByIdAndHashCodeTest() {
-        MessageDTO message1 = new MessageDTO();
-        MessageDTO message2 = new MessageDTO();
-        UUID uuid = UUID.randomUUID();
-
-        // Both the same ID
-        message1.setMessageId(uuid);
-        message1.setCreationTimestamp(CREATION_TIMESTAMP);
-        message1.setStatus(STATUS);
-        message1.setState(STATE);
-        message1.setContent(CONTENT);
-
-        message2.setMessageId(uuid);
-        message2.setCreationTimestamp(CREATION_TIMESTAMP);
-        message2.setStatus(STATUS);
-        message2.setState(STATE);
-        message2.setContent(CONTENT);
-
-        assertEquals(message1, message2);
-        assertEquals(message1.hashCode(), message2.hashCode());
-    }
-
-
 }

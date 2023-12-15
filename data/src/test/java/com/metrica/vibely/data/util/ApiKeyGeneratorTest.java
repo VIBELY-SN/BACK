81e952915cdc1462e7c139ffeefd5eecfb08c33f
gonzalorg879 <gonzalorg8799@gmail.com>
2023-12-15 09:32:10 +0100

    Created the tests for the ApiKeyGenerator

--- data/src/test/java/com/metrica/vibely/data/util/ApiKeyGeneratorTest.java
+++ data/src/test/java/com/metrica/vibely/data/util/ApiKeyGeneratorTest.java
@@ -15,4 +15,40 @@ class ApiKeyGeneratorTest {
 
     // <<-CONSTANT->>
     private static final UUID USER_ID = UUID.fromString("");
+
+    @RepeatedTest(10)
+    void generationTest() {
+        String apiKey1 = ApiKeyManager.generate(USER_ID);
+        String apiKey2 = ApiKeyManager.generate(USER_ID);
+        assertNotEquals(apiKey1, apiKey2);
+    }
+
+    @Test
+    void structureTest() {
+        String apiKey = "apiKey";
+        assertThrows(IllegalArgumentException.class, () -> ApiKeyManager.getExpirationTime(apiKey));
+    }
+
+    @Test
+    void timeExpirationTrueTest() {
+        String apiKey = ApiKeyManager.generate(USER_ID);
+
+        long expirationTime = ApiKeyManager.getExpirationTime(apiKey);
+        long currentTime = Instant.now().getEpochSecond() + 3601;
+        boolean hasExpired = currentTime > expirationTime;
+
+        assertTrue(hasExpired, "API key should have expired");
+    }
+
+    @Test
+    void timeExpirationFalseTest() {
+        String apiKey = ApiKeyManager.generate(USER_ID);
+
+        long expirationTime = ApiKeyManager.getExpirationTime(apiKey);
+        long currentTime = Instant.now().getEpochSecond();
+        boolean hasExpired = currentTime > expirationTime;
+
+        assertFalse(hasExpired, "API key should have expired");
+    }
+
 }

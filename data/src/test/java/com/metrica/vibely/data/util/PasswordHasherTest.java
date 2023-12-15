1bd1d87b9c4f0d5e72d7e937d6f14a809af47f51
gonzalorg879 <gonzalorg8799@gmail.com>
2023-12-15 09:23:08 +0100

    Created more tests for PasswordHasher

--- data/src/test/java/com/metrica/vibely/data/util/PasswordHasherTest.java
+++ data/src/test/java/com/metrica/vibely/data/util/PasswordHasherTest.java
@@ -20,4 +20,92 @@ class MessageDTOTest {
     private static final MessageStatus STATUS = MessageStatus.PENDING;
     private static final MessageState STATE = MessageState.ENABLED;
     private static final LocalDateTime CREATION_TIMESTAMP = LocalDateTime.now();
+
+    // <<-METHODS->>
+    @Test
+    @Tag("Constructors")
+    void voidConstructorTest() {
+        UUID messageId = UUID.randomUUID();
+        UUID chat = UUID.randomUUID();
+        UUID sender = UUID.randomUUID();
+
+        MessageDTO message = new MessageDTO();
+
+        message.setMessageId        (messageId);
+        message.setCreationTimestamp(CREATION_TIMESTAMP);
+        message.setStatus           (STATUS);
+        message.setState			(STATE);
+        message.setContent          (CONTENT);
+        message.setChat             (chat);
+        message.setSender           (sender);
+
+        assertEquals(messageId,          message.getMessageId());
+        assertEquals(CREATION_TIMESTAMP, message.getCreationTimestamp());
+        assertEquals(STATUS,             message.getStatus());
+        assertEquals(STATE,				 message.getState());
+        assertEquals(CONTENT,            message.getContent());
+        assertEquals(chat,               message.getChat());
+        assertEquals(sender,             message.getSender());
+    }
+
+    @Test
+    @Tag("Constructors")
+    void fullArgsConstructorTest() {
+        UUID messageId = UUID.randomUUID();
+        UUID chat = UUID.randomUUID();
+        UUID sender = UUID.randomUUID();
+
+        MessageDTO message = new MessageDTO(
+                messageId,
+                CREATION_TIMESTAMP,
+                STATUS,
+                STATE,
+                CONTENT,
+                chat,
+                sender);
+
+        assertEquals(messageId,          message.getMessageId());
+        assertEquals(CREATION_TIMESTAMP, message.getCreationTimestamp());
+        assertEquals(STATUS,             message.getStatus());
+        assertEquals(STATE,				 message.getState());
+        assertEquals(CONTENT,            message.getContent());
+        assertEquals(chat,               message.getChat());
+        assertEquals(sender,             message.getSender());
+    }
+
+    @Test
+    @Tag("Equality")
+    void basicEqualityTest() {
+        MessageDTO message = new MessageDTO();
+
+        assertEquals   (message, message);
+        assertNotEquals(message, null);
+        assertNotEquals(message, "");
+    }
+
+    @Test
+    @Tag("Equality")
+    void equalityByIdAndHashCodeTest() {
+        MessageDTO message1 = new MessageDTO();
+        MessageDTO message2 = new MessageDTO();
+        UUID uuid = UUID.randomUUID();
+
+        // Both the same ID
+        message1.setMessageId(uuid);
+        message1.setCreationTimestamp(CREATION_TIMESTAMP);
+        message1.setStatus(STATUS);
+        message1.setState(STATE);
+        message1.setContent(CONTENT);
+
+        message2.setMessageId(uuid);
+        message2.setCreationTimestamp(CREATION_TIMESTAMP);
+        message2.setStatus(STATUS);
+        message2.setState(STATE);
+        message2.setContent(CONTENT);
+
+        assertEquals(message1, message2);
+        assertEquals(message1.hashCode(), message2.hashCode());
+    }
+
+
 }

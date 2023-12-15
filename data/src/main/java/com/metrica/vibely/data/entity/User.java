960883017d30563c29210175436c9c7928260fb5
gonzalorg879 <gonzalorg8799@gmail.com>
2023-12-15 09:48:53 +0100

    Created the new Entity User

--- data/src/main/java/com/metrica/vibely/data/entity/User.java
+++ data/src/main/java/com/metrica/vibely/data/entity/User.java
@@ -1,188 +1,82 @@
 package com.metrica.vibely.data.entity;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.UUID;
-
 import com.metrica.vibely.data.model.enumerator.PrivacyType;
-import com.metrica.vibely.data.model.enumerator.Status;
+import com.metrica.vibely.data.model.enumerator.UserState;
+import com.metrica.vibely.data.model.enumerator.UserStatus;
+import com.metrica.vibely.data.util.Copyable;
+import com.metrica.vibely.data.util.DeepCopyGenerator;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
-/**
- * 
- * @since 2023-11-13
- * @version 1.0
- */
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+
 @Entity
-public class User {
-    
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Table(name = "Users")
+public class User implements Copyable<User> {
+
     // <<-FIELDS->>
 
     // Basic
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name = "userId", nullable = false)
+    @Column(name = "user_id")
     private UUID userId;
-    private String username, password, nickname, email;
-
-    @Enumerated(value = EnumType.STRING)
-    private PrivacyType privacyType;
-
-    @Enumerated(value = EnumType.STRING)
-    private Status status;
-
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    private String nickname;
+    @Column(unique = true)
+    private String email;
+    private String apikey;
+    @Enumerated(value = EnumType.ORDINAL)
+    private UserState state;
+    @Enumerated(value = EnumType.ORDINAL)
+    private PrivacyType privacy;
     private Integer logins;
+    @Enumerated(value = EnumType.ORDINAL)
+    private UserStatus status;
+    @Column(name = "last_connection_date")
+    private LocalDateTime lastConnDate;
+    @Column(name = "blocked_date")
     private LocalDate blockedDate;
 
     // Relations
-    @OneToMany
-    private List<User> followers;
-
-    @OneToMany
-    private List<User> following;
-
-    @OneToMany(mappedBy = "post_id")
-    private List<Post> posts;
-
-    @OneToMany(mappedBy = "chat_id")
-    private List<Chat> chats;
-
-    // <<-CONSTRUCTORS->>
-    public User() {
-    }
-
-    public User(UUID userId, String username, String password, String nickname, String email, List<User> followers,
-            List<User> following, List<Chat> chats, List<Post> posts, PrivacyType privacyType, Status status,
-            Integer logins, LocalDate blockedDate) {
-        this.setUserId(userId);
-        this.setUsername(username);
-        this.setPassword(password);
-        this.setNickname(nickname);
-        this.setEmail(email);
-        this.setFollowers(followers);
-        this.setFollowing(following);
-        this.setPosts(posts);
-        this.setChats(chats);
-        this.setPrivacyType(privacyType);
-        this.setStatus(status);
-        this.setLogins(logins);
-        this.setBlockedDate(blockedDate);
-    }
-
-    // <<-GETTERS & SETTERS->>
-    public UUID getUserId() {
-        return userId;
-    }
-
-    public void generateUserId() {
-        this.setUserId(UUID.randomUUID());
-    }
-
-    public void setUserId(UUID userId) {
-        this.userId = userId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public List<User> getFollowers() {
-        return followers;
-    }
-
-    public void setFollowers(List<User> followers) {
-        this.followers = followers;
-    }
-
-    public List<User> getFollowing() {
-        return following;
-    }
-
-    public void setFollowing(List<User> following) {
-        this.following = following;
-    }
-
-    public List<Post> getPosts() {
-        return posts;
-    }
-
-    public void setPosts(List<Post> posts) {
-        this.posts = posts;
-    }
-
-    public List<Chat> getChats() {
-        return chats;
-    }
-
-    public void setChats(List<Chat> chats) {
-        this.chats = chats;
-    }
-
-    public PrivacyType getPrivacyType() {
-        return privacyType;
-    }
-
-    public void setPrivacyType(PrivacyType privacyType) {
-        this.privacyType = privacyType;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-    public Integer getLogins() {
-        return logins;
-    }
-
-    public void setLogins(Integer logins) {
-        this.logins = logins;
-    }
-
-    public LocalDate getBlockedDate() {
-        return blockedDate;
-    }
-
-    public void setBlockedDate(LocalDate blockedDate) {
-        this.blockedDate = blockedDate;
-    }
+    @ManyToMany
+    @JoinTable(name = "user_follower",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "follower_id"),
+            foreignKey = @ForeignKey(name = "fk_user-follower_user"),
+            inverseForeignKey = @ForeignKey(name = "fk_user-follower_follower"))
+    private Set<User> followers;
+    @ManyToMany(mappedBy = "followers")
+    private Set<User> following;
+    @OneToMany(mappedBy = "owner")
+    private Set<Post> posts;
+    @ManyToMany(mappedBy = "participants")
+    private Set<Chat> chats;
+    @ManyToMany(mappedBy = "likedBy")
+    private Set<Post> likes;
+    @ManyToMany(mappedBy = "savedBy")
+    private Set<Post> saves;
 }

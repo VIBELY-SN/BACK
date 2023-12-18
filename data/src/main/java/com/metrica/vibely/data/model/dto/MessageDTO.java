9a1fba39ac87504d4188c8f407a4242414eed03f
Raul Valentin Radu <raulvalentinradu@gmail.com>
2023-12-18 10:12:44 +0100

    adding methods

--- data/src/main/java/com/metrica/vibely/data/model/dto/MessageDTO.java
+++ data/src/main/java/com/metrica/vibely/data/model/dto/MessageDTO.java
@@ -12,6 +12,7 @@ import java.util.UUID;
 
 import com.metrica.vibely.data.model.enumerator.MessageState;
 import com.metrica.vibely.data.model.enumerator.MessageStatus;
+
     public class MessageDTO {
 
 
@@ -40,13 +41,88 @@ import com.metrica.vibely.data.model.enumerator.MessageStatus;
                 String content,
                 UUID chat,
                 UUID sender) {
-            this.setMessageId        (messageId);
+            this.setMessageId(messageId);
             this.setCreationTimestamp(creationTimestamp);
-            this.setStatus           (status);
-            this.setState			 (state);
-            this.setContent          (content);
-            this.setChat             (chat);
-            this.setSender           (sender);
+            this.setStatus(status);
+            this.setState(state);
+            this.setContent(content);
+            this.setChat(chat);
+            this.setSender(sender);
+        }
+        //<<-METHODS->>
+
+        public MessageState getState() {
+            return state;
         }
 
+        public void setState(MessageState state) {
+            this.state = state;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(this.messageId);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (this.getClass() != obj.getClass())
+                return false;
+            com.metrica.vibely.data.model.dto.MessageDTO other = (com.metrica.vibely.data.model.dto.MessageDTO) obj;
+            return Objects.equals(this.messageId, other.messageId);
+        }
+
+        // <<-GETTERS & SETTERS->>
+        public UUID getMessageId() {
+            return this.messageId;
+        }
+
+        public void setMessageId(final UUID messageId) {
+            this.messageId = messageId;
+        }
+
+        public LocalDateTime getCreationTimestamp() {
+            return this.creationTimestamp;
+        }
+
+        public void setCreationTimestamp(final LocalDateTime creationTimestamp) {
+            this.creationTimestamp = creationTimestamp;
+        }
+
+        public MessageStatus getStatus() {
+            return this.status;
+        }
+
+        public void setStatus(final MessageStatus status) {
+            this.status = status;
+        }
+
+        public String getContent() {
+            return this.content;
+        }
+
+        public void setContent(final String content) {
+            this.content = content;
+        }
+
+        public UUID getChat() {
+            return this.chat;
+        }
+
+        public void setChat(final UUID chat) {
+            this.chat = chat;
+        }
+
+        public UUID getSender() {
+            return this.sender;
+        }
+
+        public void setSender(final UUID sender) {
+            this.sender = sender;
+        }
     }
+}

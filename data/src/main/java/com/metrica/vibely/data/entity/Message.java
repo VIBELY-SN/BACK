1f57837d6e4102a21f128987a1ee105cffca1e2c
gonzalorg879 <gonzalorg8799@gmail.com>
2023-12-18 09:12:35 +0100

    Created the new Message entity

--- data/src/main/java/com/metrica/vibely/data/entity/Message.java
+++ data/src/main/java/com/metrica/vibely/data/entity/Message.java
@@ -1,6 +1,8 @@
 package com.metrica.vibely.data.entity;
 
-import java.util.UUID;
+import com.metrica.vibely.data.model.enumerator.MessageState;
+import com.metrica.vibely.data.model.enumerator.MessageStatus;
+import com.metrica.vibely.data.util.Copyable;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,61 +11,51 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * <h1>Message Entity</h1>
+ *
+ * @since 2023-11-13
+ * @version 1.0
+ * @author Adrian, Alex, Gonzalo
+ */
 @Entity
-public class Message {
-    
-    // <<-FIELDS->>	
-	
-	//Basic
-	@Id
-	@GeneratedValue(strategy = GenerationType.UUID)
-	@Column(name = "messageId", nullable = false)
-	private UUID messageId;
-    private String srcUsername, content;
-    
-    //Relations
-	@JoinColumn(name = "chat_id", nullable = false, foreignKey = @ForeignKey(name = "fk_chat_message"))
-    private Chat chatOwner;
+public class Message implements Copyable<Message> {
 
- 
-    
+    // <<-FIELDS->>
+
+    // Basic
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "message_id")
+    private UUID messageId;
+    @Column(name = "creation_timestamp")
+    private LocalDateTime creationTimestamp;
+    private MessageStatus status;
+    private MessageState state;
+    private String content;
+
+    // Relations
+    @OneToOne(optional = false)
+    @JoinColumn(name = "chat_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_message_chat"))
+    private Chat chat;
+    @OneToOne(optional = false)
+    @JoinColumn(name = "sender_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_message_user"))
+    private User sender;
+
     // <<-CONSTRUCTORS->>
     public Message() {
+        this.setMessageId(null);
+        this.setChat(null);
+        this.setSender(null);
     }
-    
-    public Message(
-            Chat chatOwner,
-            String srcUsername,
-            String content) {
-        this.setChatOwnerId(chatOwner);
-        this.setSrcUsername(srcUsername);
-        this.setContent(content);
-    }
-
-    // <<-GETTERS & SETTERS->>
-    public Chat getChatOwner() {
-        return chatOwner;
-    }
-
-    public void setChatOwnerId(Chat chatOwner) {
-        this.chatOwner = chatOwner;
-    }
-
-    public String getSrcUsername() {
-        return srcUsername;
-    }
-
-    public void setSrcUsername(String srcUsername) {
-        this.srcUsername = srcUsername;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-    
 }

2f7b0a747fc6fa5f44528e8d09712bda4e0205dd
gonzalorg879 <gonzalorg8799@gmail.com>
2023-12-18 16:20:05 +0100

    created the new ChatMapper

---
+++ data/src/main/java/com/metrica/vibely/data/model/mapper/ChatMapper.java
@@ -0,0 +1,12 @@
+package com.metrica.vibely.data.model.mapper;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.metrica.vibely.data.entity.Chat;
+import com.metrica.vibely.data.entity.Message;
+import com.metrica.vibely.data.entity.User;
+import com.metrica.vibely.data.model.dto.ChatDTO;
+
+public class ChatMapper {
+}

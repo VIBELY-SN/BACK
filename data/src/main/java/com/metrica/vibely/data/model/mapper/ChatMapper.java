27bceb389557a5b7f67bfb6bac6be2e72e8c9474
gonzalorg879 <gonzalorg8799@gmail.com>
2023-12-18 16:20:58 +0100

    created method toEntity

--- data/src/main/java/com/metrica/vibely/data/model/mapper/ChatMapper.java
+++ data/src/main/java/com/metrica/vibely/data/model/mapper/ChatMapper.java
@@ -9,4 +9,25 @@ import com.metrica.vibely.data.entity.User;
 import com.metrica.vibely.data.model.dto.ChatDTO;
 
 public class ChatMapper {
+
+    public static Chat toEntity(ChatDTO chatDTO, Set<User> participants, Set<Message> messages) {
+        Chat chat = new Chat();
+
+        // Mapping Basics
+        chat.setChatId		(chatDTO.getChatId());
+        chat.setCreationDate(chatDTO.getCreationDate());
+        chat.setType		(chatDTO.getType());
+        chat.setStatus		(chatDTO.getStatus());
+        chat.setState		(chatDTO.getState());
+        chat.setTitle		(chatDTO.getTitle());
+        chat.setLastActivity(chatDTO.getLastActivity());
+
+        // Mapping Relations
+        chat.setParticipants(participants);
+        chat.setMessages	(messages);
+
+        return chat;
+    }
+
+
 }

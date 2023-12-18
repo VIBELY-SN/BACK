d56491e6887dfeacd891d8f519a134c0320715fb
gonzalorg879 <gonzalorg8799@gmail.com>
2023-12-18 15:48:43 +0100

    created AdminMapper

---
+++ data/src/main/java/com/metrica/vibely/data/model/mapper/AdminMapper.java
@@ -0,0 +1,12 @@
+package com.metrica.vibely.data.model.mapper;
+
+import com.metrica.vibely.data.entity.Admin;
+import com.metrica.vibely.data.entity.Chat;
+import com.metrica.vibely.data.entity.Post;
+import com.metrica.vibely.data.entity.User;
+import com.metrica.vibely.data.model.dto.AdminDTO;
+
+import java.util.stream.Collectors;
+
+public class AdminMapper {
+}

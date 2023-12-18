dba55590744114f2b58268339e0e7c63f377d7eb
gonzalorg879 <gonzalorg8799@gmail.com>
2023-12-18 10:16:25 +0100

    Created ChatDTO with getters & setters

---
+++ data/src/main/java/com/metrica/vibely/data/model/dto/ChatDTO.java
@@ -0,0 +1,127 @@
+package com.metrica.vibely.data.model.dto;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+
+import com.metrica.vibely.data.model.enumerator.ChatState;
+import com.metrica.vibely.data.model.enumerator.ChatStatus;
+import com.metrica.vibely.data.model.enumerator.ChatType;
+
+public class ChatDTO {
+
+    // <<-FIELDS->>
+
+    // Basic
+    private UUID chatId;
+    private LocalDateTime creationDate;
+    private ChatType type;
+    private ChatStatus status;
+    private ChatState state;
+    private String title;
+    private LocalDateTime lastActivity;
+
+    // Relations
+    private Set<UUID> participants;
+    private Set<UUID> messages;
+
+
+    // <<-CONSTRUCTORS->>
+    public ChatDTO() {
+    }
+
+    public ChatDTO(
+            UUID chatId,
+            LocalDateTime creationDate,
+            ChatType type,
+            ChatStatus status,
+            ChatState state,
+            String title,
+            LocalDateTime lastActivity,
+            Set<UUID> participants,
+            Set<UUID> messages) {
+        this.chatId = chatId;
+        this.creationDate = creationDate;
+        this.type = type;
+        this.status = status;
+        this.state = state;
+        this.title = title;
+        this.lastActivity = lastActivity;
+        this.participants = participants;
+        this.messages = messages;
+    }
+
+    // <<-GETTERS & SETTERS->>
+    public UUID getChatId() {
+        return this.chatId;
+    }
+
+    public void setChatId(final UUID chatId) {
+        this.chatId = chatId;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return this.creationDate;
+    }
+
+    public void setCreationDate(final LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public ChatType getType() {
+        return this.type;
+    }
+
+    public void setType(final ChatType type) {
+        this.type = type;
+    }
+
+    public ChatStatus getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(final ChatStatus status) {
+        this.status = status;
+    }
+
+    public ChatState getState() {
+        return this.state;
+    }
+
+    public void setState(final ChatState state) {
+        this.state = state;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(final String title) {
+        this.title = title;
+    }
+
+    public LocalDateTime getLastActivity() {
+        return this.lastActivity;
+    }
+
+    public void setLastActivity(final LocalDateTime lastActivity) {
+        this.lastActivity = lastActivity;
+    }
+
+    public Set<UUID> getParticipants() {
+        return this.participants;
+    }
+
+    public void setParticipants(final Set<UUID> participants) {
+        this.participants = participants;
+    }
+
+    public Set<UUID> getMessages() {
+        return this.messages;
+    }
+
+    public void setMessages(final Set<UUID> messages) {
+        this.messages = messages;
+    }
+}

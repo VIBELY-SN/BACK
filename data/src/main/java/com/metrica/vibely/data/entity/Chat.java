d482fa06fffba30f3d7ed7d368715d04a4e3a9b1
Raul Valentin Radu <raulvalentinradu@gmail.com>
2023-12-18 09:21:59 +0100

    adding fields and jpa relations

--- data/src/main/java/com/metrica/vibely/data/entity/Chat.java
+++ data/src/main/java/com/metrica/vibely/data/entity/Chat.java
@@ -1,5 +1,67 @@
 package com.metrica.vibely.data.entity;
 
-public class Chat {
+import com.metrica.vibely.data.model.enumerator.ChatType;
+import jakarta.persistence.*;
+
+    @Entity
+    public class Chat implements Copyable<com.metrica.vibely.data.entity.Chat> {
+
+        // <<-FIELDS->>
+
+        // Basic
+        @Id
+        @GeneratedValue(strategy = GenerationType.UUID)
+        @Column(name = "chat_id")
+        private UUID chatId;
+        @Column(name = "creation_date")
+        private LocalDateTime creationDate;
+        @Enumerated(value = EnumType.ORDINAL)
+        private ChatType type;
+        @Enumerated(value = EnumType.ORDINAL)
+        private ChatStatus status;
+        @Enumerated(value = EnumType.ORDINAL)
+        private ChatState state;
+        private String title;
+        @Column(name = "last_activity")
+        private LocalDateTime lastActivity;
+
+        // Relations
+        @ManyToMany
+        @JoinTable(name = "user_chat",
+                joinColumns = @JoinColumn(name = "chat_id"),
+                inverseJoinColumns = @JoinColumn(name = "user_id"),
+                foreignKey = @ForeignKey(name = "fk_user-chat_chat"),
+                inverseForeignKey = @ForeignKey(name = "fk_user-chat_user"))
+        private Set<User> participants;
+        @OneToMany(mappedBy = "chat")
+        private Set<Message> messages;
+
+        // <<-CONSTRUCTORS->>
+        public Chat() {
+            this.setChatId(null);
+            this.setParticipants(null);
+            this.setMessages(null);
+        }
+
+        public Chat(
+                UUID chatId,
+                LocalDateTime lastActivity,
+                ChatType type,
+                ChatStatus status,
+                ChatState state,
+                String title,
+                LocalDateTime creationDate,
+                Set<User> participants,
+                Set<Message> messages) {
+            this.setChatId      (chatId);
+            this.setLastActivity(lastActivity);
+            this.setType        (type);
+            this.setStatus      (status);
+            this.setState		(state);
+            this.setTitle       (title);
+            this.setCreationDate(creationDate);
+            this.setParticipants(participants);
+            this.setMessages    (messages);
+        }
 
 }

e8de74c6b40e6a3bfaa1cebfdd279b075ba8b3cb
gonzalorg879 <gonzalorg8799@gmail.com>
2023-12-18 15:54:34 +0100

    created method toDto for AdminMapper

--- data/src/main/java/com/metrica/vibely/data/model/mapper/AdminMapper.java
+++ data/src/main/java/com/metrica/vibely/data/model/mapper/AdminMapper.java
@@ -50,5 +50,56 @@ public class AdminMapper {
         return admin;
     }
 
+    /**
+     * Convert an entity into a DTO
+     *
+     * @param admin entity
+     * @return the admin DTO
+     */
+    public static AdminDTO toDTO(Admin admin) {
+        AdminDTO adminDTO = new AdminDTO();
+
+        // Mapping Basics
+        adminDTO.setUserId		(admin.getUserId());
+        adminDTO.setUsername	(admin.getUsername());
+        adminDTO.setPassword	(admin.getPassword());
+        adminDTO.setNickname	(admin.getNickname());
+        adminDTO.setEmail		(admin.getEmail());
+        adminDTO.setApikey		(admin.getApikey());
+        adminDTO.setState		(admin.getState());
+        adminDTO.setPrivacy		(admin.getPrivacy());
+        adminDTO.setLogins		(admin.getLogins());
+        adminDTO.setStatus		(admin.getStatus());
+        adminDTO.setLastConnDate(admin.getLastConnDate());
+        adminDTO.setBlockedDate (admin.getBlockedDate());
+
+        // Mapping Relations
+        adminDTO.setFollowers(admin.getFollowers()
+                .stream()
+                .map(User::getUserId)
+                .collect(Collectors.toSet()));
+        adminDTO.setFollowing(admin.getFollowing()
+                .stream()
+                .map(User::getUserId)
+                .collect(Collectors.toSet()));
+        adminDTO.setPosts(admin.getPosts()
+                .stream()
+                .map(Post::getPostId)
+                .collect(Collectors.toSet()));
+        adminDTO.setChats(admin.getChats()
+                .stream()
+                .map(Chat::getChatId)
+                .collect(Collectors.toSet()));
+        adminDTO.setLikes(admin.getLikes()
+                .stream()
+                .map(Post::getPostId)
+                .collect(Collectors.toSet()));
+        adminDTO.setSaves(admin.getSaves()
+                .stream()
+                .map(Post::getPostId)
+                .collect(Collectors.toSet()));
+
+        return adminDTO;
+    }
 
 }

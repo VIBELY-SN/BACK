8d10c8a5b8670b119432e7a3d032a75b6a321aca
Raul Valentin Radu <raulvalentinradu@gmail.com>
2023-12-18 10:16:21 +0100

    adding methods

--- data/src/main/java/com/metrica/vibely/data/model/dto/PostDTO.java
+++ data/src/main/java/com/metrica/vibely/data/model/dto/PostDTO.java
@@ -58,4 +58,132 @@ public class PostDTO {
         this.likedBy = likedBy;
         this.savedBy = savedBy;
     }
+
+    // <<-GETTERS & SETTERS->>
+    public UUID getPostId() {
+        return this.postId;
+    }
+
+
+    public void setPostId(final UUID postId) {
+        this.postId = postId;
+    }
+
+
+    public LocalDateTime getPostDate() {
+        return this.postDate;
+    }
+
+
+    public void setPostDate(final LocalDateTime postDate) {
+        this.postDate = postDate;
+    }
+
+
+    public PostStatus getStatus() {
+        return this.status;
+    }
+
+
+    public void setStatus(final PostStatus status) {
+        this.status = status;
+    }
+
+
+    public PostVisibility getVisibility() {
+        return this.visibility;
+    }
+
+
+    public void setVisibility(final PostVisibility visibility) {
+        this.visibility = visibility;
+    }
+
+
+    public String getContent() {
+        return this.content;
+    }
+
+
+    public void setContent(final String content) {
+        this.content = content;
+    }
+
+
+    public Integer getLikes() {
+        return this.likes;
+    }
+
+
+    public void setLikes(final Integer likes) {
+        this.likes = likes;
+    }
+
+
+    public Integer getTimesSaved() {
+        return this.timesSaved;
+    }
+
+
+    public void setTimesSaved(final Integer timesSaved) {
+        this.timesSaved = timesSaved;
+    }
+
+
+    public UUID getOwner() {
+        return this.owner;
+    }
+
+
+    public void setOwner(final UUID owner) {
+        this.owner = owner;
+    }
+
+
+    public Set<UUID> getComments() {
+        return this.comments;
+    }
+
+
+    public void setComments(final Set<UUID> comments) {
+        this.comments = comments;
+    }
+
+
+    public Set<UUID> getLikedBy() {
+        return this.likedBy;
+    }
+
+
+    public void setLikedBy(final Set<UUID> likedBy) {
+        this.likedBy = likedBy;
+    }
+
+
+    public Set<UUID> getSavedBy() {
+        return this.savedBy;
+    }
+
+
+    public void setSavedBy(final Set<UUID> savedBy) {
+        this.savedBy = savedBy;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.postId);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (this.getClass() != obj.getClass())
+            return false;
+        PostDTO other = (PostDTO) obj;
+        return Objects.equals(this.postId, other.postId);
+    }
+
 }

e4c7f4cb2b130ac03c6626e95d0eb7a0b01b3626
gonzalorg879 <gonzalorg8799@gmail.com>
2023-12-21 10:50:34 +0100

    Created the methods findByUsername and findByEmail in AdminRepository

--- data/src/main/java/com/metrica/vibely/data/repository/AdminRepository.java
+++ data/src/main/java/com/metrica/vibely/data/repository/AdminRepository.java
@@ -12,5 +12,21 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, java.util.UUID> {
 
+
+    /**
+     * Find a user by its username.
+     *
+     * @param username the unique username
+     * @return the user if exist
+     */
+    Optional<Admin> findByUsername(String username);
+
+    /**
+     * Find a user by its username.
+     *
+     * @param username the unique username
+     * @return the user if exist
+     */
+    Optional<Admin> findByEmail(String email);
 }
 

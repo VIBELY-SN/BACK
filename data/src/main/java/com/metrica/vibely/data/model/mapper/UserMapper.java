1bd7047664a548727561da53ffa2e054b1ac0ee9
gonzalorg879 <gonzalorg8799@gmail.com>
2023-12-21 11:07:46 +0100

    Created the new class UserMapper

--- data/src/main/java/com/metrica/vibely/data/model/mapper/UserMapper.java
+++ data/src/main/java/com/metrica/vibely/data/model/mapper/UserMapper.java
@@ -1,29 +1,50 @@
 package com.metrica.vibely.data.model.mapper;
 
+import com.metrica.vibely.data.entity.Chat;
+import com.metrica.vibely.data.entity.Post;
 import com.metrica.vibely.data.entity.User;
 import com.metrica.vibely.data.model.dto.UserDTO;
 
+import java.util.stream.Collectors;
+
 public class UserMapper {
 
-	public static User toEntity(UserDTO userdto) {
+	// <<-METHODS->>
+	/**
+	 * Maps the information contained in the DTO into the Entity.
+	 *
+	 * This method is mainly used when you create a new entity
+	 * so it's really not important the values you assign to the
+	 * relations fields.
+	 *
+	 * @param userDTO the DTO to map
+	 * @return the entity with the DTO info
+	 */
+	public static User toEntity(UserDTO userDTO) {
 		User user = new User();
-		
-		user.setNickname(userdto.getNickname());
-		user.setUsername(userdto.getUsername());
-		user.setEmail(userdto.getEmail());
-		user.setPassword(userdto.getPassword());
-		
+
+		// Mapping Basics
+		user.setUserId		(userDTO.getUserId());
+		user.setNickname	(userDTO.getNickname());
+		user.setUsername	(userDTO.getUsername());
+		user.setPassword	(userDTO.getPassword());
+		user.setEmail		(userDTO.getEmail());
+		user.setApikey		(userDTO.getApikey());
+		user.setPrivacy		(userDTO.getPrivacy());
+		user.setStatus		(userDTO.getStatus());
+		user.setState		(userDTO.getState());
+		user.setLogins		(userDTO.getLogins());
+		user.setBlockedDate (userDTO.getBlockedDate());
+		user.setLastConnDate(userDTO.getLastConnDate());
+
+		// Mapping Relations
+		user.setFollowers(null);
+		user.setFollowing(null);
+		user.setPosts    (null);
+		user.setChats    (null);
+		user.setLikes    (null);
+		user.setSaves    (null);
+
 		return user;
-	}
-	
-	public static UserDTO toDTO(User user) {
-		UserDTO userdto = new UserDTO();
-		
-		userdto.setNickname(user.getNickname());
-		userdto.setUsername(user.getUsername());
-		userdto.setEmail(user.getEmail());
-		userdto.setPassword(user.getPassword());
-		
-		return userdto;
 	}
 }

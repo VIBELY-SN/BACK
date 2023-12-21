132f527add173054a6d3f4f646073a31b0334857
gonzalorg879 <gonzalorg8799@gmail.com>
2023-12-21 10:49:05 +0100

    Created the new AdminRepository

--- data/src/main/java/com/metrica/vibely/data/repository/AdminRepository.java
+++ data/src/main/java/com/metrica/vibely/data/repository/AdminRepository.java
@@ -1,11 +1,16 @@
 package com.metrica.vibely.data.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.metrica.vibely.data.entity.Admin;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, java.util.UUID> {
 
 }
+

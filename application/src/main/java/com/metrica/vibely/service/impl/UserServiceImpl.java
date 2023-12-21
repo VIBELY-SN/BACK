79a23d6f96ca19165ad051e6b29bb046ced48c78
gonzalorg879 <gonzalorg8799@gmail.com>
2023-12-21 12:37:15 +0100

    created the new UserServiceImpl

--- application/src/main/java/com/metrica/vibely/service/impl/UserServiceImpl.java
+++ application/src/main/java/com/metrica/vibely/service/impl/UserServiceImpl.java
@@ -1,4 +1,39 @@
 package com.metrica.vibely.service.impl;
 
-public class UserServiceImpl {
+import com.metrica.vibely.data.entity.User;
+import com.metrica.vibely.data.model.dto.UserDTO;
+import com.metrica.vibely.data.model.enumerator.PrivacyType;
+import com.metrica.vibely.data.model.enumerator.UserState;
+import com.metrica.vibely.data.model.enumerator.UserStatus;
+import com.metrica.vibely.data.model.mapper.UserMapper;
+import com.metrica.vibely.data.entity.Chat;
+import com.metrica.vibely.data.entity.Post;
+import com.metrica.vibely.data.repository.UserRepository;
+import com.metrica.vibely.data.util.PasswordHasher;
+import com.metrica.vibely.service.UserService;
+
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    // <<-FIELD->>
+    private UserRepository userRepository;
+
+    // <<-CONSTRUCTOR->>
+    @Autowired
+    public UserServiceImpl(final UserRepository userRepostory) {
+        this.userRepository = userRepostory;
+    }
 }

094d5145032e778f4cea5fc1ee01e670db372d37
gonzalorg879 <gonzalorg8799@gmail.com>
2023-12-28 12:25:32 +0100

    created AddRemoveChatRequest class

--- application/src/main/java/com/metrica/vibely/model/request/AddRemoveChatRequest.java
+++ application/src/main/java/com/metrica/vibely/model/request/AddRemoveChatRequest.java
@@ -1,4 +1,24 @@
 package com.metrica.vibely.model.request;
 
+import java.util.Set;
+import java.util.UUID;
+
+import com.metrica.vibely.data.model.dto.ChatDTO;
+
 public class AddRemoveChatRequest {
+//  <<--ATRIBUTES-->>
+    Set<UUID> participants;
+
+//  <<--CONSTRUCTOR-->>
+    public AddRemoveChatRequest() {
+    }
+
+//  <<--GETTERS & SETTERS-->>
+    public Set<UUID> getParticipants() {
+    return participants;
+    }
+
+    public void setParticipants(Set<UUID> participants) {
+        this.participants = participants;
+    }
 }

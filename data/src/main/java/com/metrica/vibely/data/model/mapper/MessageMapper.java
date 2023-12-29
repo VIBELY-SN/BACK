9b9a74e8f0b54c4e3487cfbf55606952d9347c85
Raul Valentin Radu <raulvalentinradu@gmail.com>
2023-12-29 11:00:49 +0100

    implementing toEntity method

--- data/src/main/java/com/metrica/vibely/data/model/mapper/MessageMapper.java
+++ data/src/main/java/com/metrica/vibely/data/model/mapper/MessageMapper.java
@@ -1,5 +1,20 @@
 package com.metrica.vibely.data.model.mapper;
 
 public class MessageMapper {
+	public static Message toEntity(MessageDTO messageDTO, Chat chat, User sender) {
+        Message message = new Message();
 
+        // Mapping Basics
+        message.setMessageId		(messageDTO.getMessageId());
+        message.setCreationTimestamp(messageDTO.getCreationTimestamp());
+        message.setStatus			(messageDTO.getStatus());
+        message.setState			(messageDTO.getState());
+        message.setContent			(messageDTO.getContent());
+
+        // Mapping Relations
+        message.setChat				(chat);
+        message.setSender			(sender);
+
+        return message;
+    }
 }

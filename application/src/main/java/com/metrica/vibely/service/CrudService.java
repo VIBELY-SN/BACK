a9754a03eee2b91335bba6aed58ce6943fc1e6f9
Raul Valentin Radu <raulvalentinradu@gmail.com>
2023-12-29 12:26:30 +0100

    adding CRUD methods

--- application/src/main/java/com/metrica/vibely/service/CrudService.java
+++ application/src/main/java/com/metrica/vibely/service/CrudService.java
@@ -1,5 +1,36 @@
 package com.metrica.vibely.service;
 
-public class CrudService {
+public interface CrudService<DTO, ID> {
+	/**
+     * Fetches an object by its identifier.
+     *
+     * @param id the object identifier
+     * @return the object DTO
+     * @throws NoSuchElementException
+     */
+    DTO getById(ID id);
 
+    /**
+     * Creates an object using provided information.
+     * 
+     * @param dto the information to create
+     * @return the object DTO
+     */
+    DTO create(DTO dto);
+
+    /**
+     * Updates an object using provided information.
+     * 
+     * @param dto the information to update
+     * @return the object DTO
+     */
+    DTO update(DTO dto);
+
+    /**
+     * Deletes an object by its identifier.
+     * 
+     * @param id the object identifier
+     * @throws NoSuchElementException
+     */
+    void deleteById(ID id);
 }

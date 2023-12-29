770f7b772f49e15165858f1c251a12b2805d39a4
Raul Valentin Radu <raulvalentinradu@gmail.com>
2023-12-29 12:34:48 +0100

    implementing methods

--- application/src/main/java/com/metrica/vibely/service/AdminService.java
+++ application/src/main/java/com/metrica/vibely/service/AdminService.java
@@ -1,5 +1,54 @@
 package com.metrica.vibely.service;
 
-public interface AdminService {
+import com.metrica.vibely.data.model.dto.AdminDTO;
+import java.util.UUID;
 
+
+public interface AdminService extends CrudService<AdminDTO, UUID> {
+	
+	/**
+	 * Gets an Admininistrator by its username.
+	 * 
+	 * @param username the username to search
+	 * @return the admin DTO
+	 * @throws NoSuchElementException in case the admin does not exist
+	 */
+	AdminDTO getByUsername(String username);	
+    
+    /**
+     * Gets an Admininistrator by its email.
+     * 
+     * @param username the username to search
+     * @return the admin DTO
+     * @throws NoSuchElementException in case the admin does not exist
+     */
+    AdminDTO getByEmail(String email);
+	
+    /**
+     * Deletes an admin given its username
+     * 
+     * @param username the username to delete
+     * @throws NoSuchElementException
+     */
+    void deleteByUsername(String username);
+	
+	/**
+	 * 
+	 * @param adminId 
+	 * @param follwedUserId
+	 * @param username
+	 * @return
+	 * @throws NoSuchElementException
+	 */
+	AdminDTO followUser(UUID adminId, UUID followedUserId);
+	
+	/**
+	 * 
+	 * @param adminId
+	 * @param follwedUserId
+	 * @return
+	 * @throws NoSuchElementException
+	 */
+	AdminDTO unfollowUser(UUID adminId, UUID followedUserId);
+	
 }

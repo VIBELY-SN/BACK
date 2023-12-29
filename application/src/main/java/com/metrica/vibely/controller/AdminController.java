00026210a90f66ec0f1e688a5a4cf9395d392245
Raul Valentin Radu <raulvalentinradu@gmail.com>
2023-12-29 13:02:50 +0100

    adding imports, atributes and creating constructor

--- application/src/main/java/com/metrica/vibely/controller/AdminController.java
+++ application/src/main/java/com/metrica/vibely/controller/AdminController.java
@@ -1,5 +1,41 @@
 package com.metrica.vibely.controller;
 
-public class AdminController {
+import com.metrica.vibely.controller.util.ResponseManager;
+import com.metrica.vibely.data.model.dto.ChatDTO;
+import com.metrica.vibely.data.model.enumerator.ChatState;
+import com.metrica.vibely.model.request.AddRemoveChatRequest;
+import com.metrica.vibely.model.request.CreateChatRequest;
+import com.metrica.vibely.model.request.UpdateChatRequest;
+import com.metrica.vibely.model.response.create.CreateChatResponse;
+import com.metrica.vibely.model.response.get.GetChatResponse;
+import com.metrica.vibely.model.response.update.UpdateChatResponse;
+import com.metrica.vibely.service.ChatService;
 
+import jakarta.validation.Valid;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class AdminController {
+		// ATRIBUTEs
+		ResponseManager responseManager;
+	    private ChatService chatService;
+
+	    // CONSTRUCTOR
+	    @Autowired
+	    public ChatController(ChatService chatService, ResponseManager responseManager) {
+	    	this.responseManager = responseManager;
+	        this.chatService = chatService;
+	    }
 }

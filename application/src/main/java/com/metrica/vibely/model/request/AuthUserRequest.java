3bb7cc192bcc4295509ff715fee88819e85f7af0
Raul Valentin Radu <raulvalentinradu@gmail.com>
2023-12-29 14:04:57 +0100

    implementing restrictions

--- application/src/main/java/com/metrica/vibely/model/request/AuthUserRequest.java
+++ application/src/main/java/com/metrica/vibely/model/request/AuthUserRequest.java
@@ -1,5 +1,39 @@
 package com.metrica.vibely.model.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public class AuthUserRequest {
+
+    // <<-FIELDS->>
+    @NotNull
+    @NotBlank
+    private String username;
+    @NotNull
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*#?&]{12,}$", message = "contraseña no valida")
+    private String password;
+
+    // <<-CONSTRUCTOR->>
+    public AuthUserRequest() {
+    }
+
+    // <<-GETTERS & SETTERS->>
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }

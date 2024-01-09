9f33fdb0aebef277b075a2b9d4a65eebab9eaf07
Tiago <Tiago@DESKTOP-8CM33AL>
2024-01-09 14:36:26 +0100

    AdminService finished

--- data/src/main/java/com/metrica/vibely/data/service/AdminService.java
+++ data/src/main/java/com/metrica/vibely/data/service/AdminService.java
@@ -1,5 +1,25 @@
 package com.metrica.vibely.data.service;
 
-public interface AdminService {
+import com.metrica.vibely.data.model.dto.AdminDTO;
 
+import java.util.UUID;
+
+
+public interface AdminService extends CrudService<AdminDTO, UUID> {
+	
+	
+	AdminDTO getByUsername(String username);	
+    
+    
+    AdminDTO getByEmail(String email);
+	
+    
+    void deleteByUsername(String username);
+	
+	
+	AdminDTO followUser(UUID adminId, UUID followedUserId);
+	
+	
+	AdminDTO unfollowUser(UUID adminId, UUID followedUserId);
+	
 }

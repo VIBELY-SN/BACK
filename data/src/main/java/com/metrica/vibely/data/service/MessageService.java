33c0317e896e5834a767fb447ccd35ac83b2cf0f
Tiago <Tiago@DESKTOP-8CM33AL>
2024-01-09 14:49:50 +0100

    MessageService finished

--- data/src/main/java/com/metrica/vibely/data/service/MessageService.java
+++ data/src/main/java/com/metrica/vibely/data/service/MessageService.java
@@ -1,5 +1,20 @@
 package com.metrica.vibely.data.service;
 
-public interface MessageService {
+import java.util.UUID;
+
+import com.metrica.vibely.data.model.dto.ChatDTO;
+import com.metrica.vibely.data.model.dto.MessageDTO;
+import com.metrica.vibely.data.model.dto.UserDTO;
+
+
+public interface MessageService extends CrudService<MessageDTO, UUID> {
+
+	MessageDTO getByContent(String content);	
+
+	
+	UUID getSender(UUID messageId);
+	
+	
+	UUID getChat(UUID messageId);	
 
 }

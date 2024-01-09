aa0c923adcc6d10a1020e7a05a214993f46bbf9f
Tiago <Tiago@DESKTOP-8CM33AL>
2024-01-09 14:56:43 +0100

    PostService finished

--- data/src/main/java/com/metrica/vibely/data/service/PostService.java
+++ data/src/main/java/com/metrica/vibely/data/service/PostService.java
@@ -1,5 +1,21 @@
 package com.metrica.vibely.data.service;
 
-public interface PostService {
+import java.util.UUID;
 
+import com.metrica.vibely.data.model.dto.PostDTO;
+import com.metrica.vibely.data.model.enumerator.PostStatus;
+import com.metrica.vibely.data.model.enumerator.PostVisibility;
+
+
+public interface PostService extends CrudService<PostDTO, UUID> {
+	
+	PostDTO getByContentandCreator(String content, String username);
+	
+	
+	PostDTO changeStatus(PostStatus status); 
+	
+	
+	PostDTO changeVisibility(PostVisibility visibility); 
+
+	void deleteById(UUID postId);
 }

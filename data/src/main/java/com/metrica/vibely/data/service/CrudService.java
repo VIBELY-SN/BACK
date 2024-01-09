2a56a549cf271d4699f5aa09b4dddfb612e3c2f4
Tiago <Tiago@DESKTOP-8CM33AL>
2024-01-09 14:55:24 +0100

    CrudService finished

--- data/src/main/java/com/metrica/vibely/data/service/CrudService.java
+++ data/src/main/java/com/metrica/vibely/data/service/CrudService.java
@@ -1,5 +1,18 @@
 package com.metrica.vibely.data.service;
 
-public interface CrudService {
+
+abstract interface CrudService<DTO, ID> {
+
+    
+    DTO getById(ID id);
+
+    
+    DTO create(DTO dto);
+
+    
+    DTO update(DTO dto);
+
+   
+    void deleteById(ID id);
 
 }

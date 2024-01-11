40fca7bdf2b839770d7e94259fae739cc679aba7
gonzalorg879 <gonzalorg8799@gmail.com>
2024-01-11 11:37:20 +0100

    add methods to AppConfiguration class

--- application/src/main/java/com/metrica/vibely/conf/AppConfiguration.java
+++ application/src/main/java/com/metrica/vibely/conf/AppConfiguration.java
@@ -31,4 +31,21 @@ public class AppConfiguration implements WebMvcConfigurer {
         this.authInterceptor = authInterceptor;
         this.adminAuthInterceptor = adminAuthInterceptor;
     }
+
+
+    // <<-METHOD->>
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(this.authInterceptor)
+                .addPathPatterns("/api/v1/**")
+                .excludePathPatterns("/api/v1/auth/**")
+                .excludePathPatterns("/api/v1/users/signup")
+                .excludePathPatterns("/api/v1/admin/**");
+
+        registry.addInterceptor(this.adminAuthInterceptor)
+                .addPathPatterns("/api/v1/admin/**")
+                .excludePathPatterns("/api/v1/admin/auth/**")
+                .excludePathPatterns("/api/v1/admin/users/signup");
+    }
+
 }

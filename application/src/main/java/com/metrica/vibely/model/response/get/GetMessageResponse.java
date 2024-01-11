fa5b58470ffc82e0daa7392e725f5e4ed1498bdf
gonzalorg879 <gonzalorg8799@gmail.com>
2024-01-11 11:53:15 +0100

    Created GetMessageResponse class

---
+++ application/src/main/java/com/metrica/vibely/model/response/get/GetMessageResponse.java
@@ -0,0 +1,82 @@
+package com.metrica.vibely.model.response.get;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.metrica.vibely.data.model.dto.MessageDTO;
+import com.metrica.vibely.data.model.enumerator.MessageState;
+import com.metrica.vibely.data.model.enumerator.MessageStatus;
+
+public class GetMessageResponse {
+
+    //	<<--ATRIBUTES-->>
+    private UUID messageId;
+    private LocalDateTime creationTimestamp;
+    private MessageStatus status;
+    private MessageState state;
+    private String content;
+
+    private UUID chat;
+    private UUID sender;
+
+    //	<<--CONSTRUCTOR-->>
+    public GetMessageResponse() {
+    }
+
+    //	<<--GETTERS & SETTERS-->>
+    public UUID getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(UUID messageId) {
+        this.messageId = messageId;
+    }
+
+    public LocalDateTime getCreationTimestamp() {
+        return creationTimestamp;
+    }
+
+    public void setCreationTimestamp(LocalDateTime creationTimestamp) {
+        this.creationTimestamp = creationTimestamp;
+    }
+
+    public MessageStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(MessageStatus status) {
+        this.status = status;
+    }
+
+    public MessageState getState() {
+        return state;
+    }
+
+    public void setState(MessageState state) {
+        this.state = state;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public UUID getChat() {
+        return chat;
+    }
+
+    public void setChat(UUID chat) {
+        this.chat = chat;
+    }
+
+    public UUID getSender() {
+        return sender;
+    }
+
+    public void setSender(UUID sender) {
+        this.sender = sender;
+    }
+}

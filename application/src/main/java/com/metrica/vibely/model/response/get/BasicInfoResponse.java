2e0ab9c990b76325c0fafe97c49cb24e4168f793
Raul <Raul@DESKTOP-T5U920N>
2024-01-11 12:00:33 +0100

    atriubtes + constructor

--- application/src/main/java/com/metrica/vibely/model/response/get/BasicInfoResponse.java
+++ application/src/main/java/com/metrica/vibely/model/response/get/BasicInfoResponse.java
@@ -1,4 +1,26 @@
 package com.metrica.vibely.model.response.get;
 
+import com.metrica.vibely.data.model.dto.UserDTO;
+import com.metrica.vibely.data.model.enumerator.PrivacyType;
+import com.metrica.vibely.data.model.enumerator.UserState;
+
+import java.util.Set;
+import java.util.UUID;
+
 public class BasicInfoResponse {
+	// <<-FIELDS->>
+    private UUID userId;
+    private String username;
+    private String nickname;
+    private String email;
+    private UserState state;
+    private PrivacyType privacy;
+    private Set<UUID> followers;
+    private Set<UUID> following;
+    private Set<UUID> posts;
+    
+    // <<-CONSTRUCTOR->>
+    public BasicInfoResponse() {
+    }
+	
 }

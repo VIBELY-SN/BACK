da3beb1684bd339507b7e6587eb4bd910ceaffed
Tiago <Tiago@DESKTOP-8CM33AL>
2024-01-11 11:18:37 +0100

    UpdatePostRequest finished

--- application/src/main/java/com/metrica/vibely/model/request/UpdatePostRequest.java
+++ application/src/main/java/com/metrica/vibely/model/request/UpdatePostRequest.java
@@ -1,5 +1,45 @@
 package com.metrica.vibely.model.request;
 
-public class UpdatePostRequest {
+import com.metrica.vibely.data.model.dto.PostDTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * <h1>Update Post Request</h1>
+ * 
+ * @since 2023-11-28
+ * @version 1.0
+ * @author 
+ */
+public class UpdatePostRequest {
+	
+	// <<-FIELDS->>
+	@NotNull
+	@NotBlank
+	private String content;
+	
+	// <<-CONSTRUCTOR->>
+    public UpdatePostRequest() {
+    }
+    
+    // <<-METHODS->>
+    public PostDTO toDTO() {
+    	PostDTO updateDTO = new PostDTO();
+        
+    	updateDTO.setContent(this.content);
+        
+        return updateDTO;
+    }
+
+    // <<-GETTERS & SETTERS->>
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+    
+	
 }

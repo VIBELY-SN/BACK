a68336c58a79fdf0b4ee191514fa620f0ed14732
Tiago <Tiago@DESKTOP-8CM33AL>
2024-01-11 10:26:53 +0100

    adding constructor and some methods to PostController

--- application/src/main/java/com/metrica/vibely/controller/PostController.java
+++ application/src/main/java/com/metrica/vibely/controller/PostController.java
@@ -1,5 +1,73 @@
 package com.metrica.vibely.controller;
 
+import com.metrica.vibely.controller.util.ResponseManager;
+import com.metrica.vibely.data.model.dto.PostDTO;
+import com.metrica.vibely.model.request.CreatePostRequest;
+import com.metrica.vibely.model.request.UpdateLikedByPostRequest;
+import com.metrica.vibely.model.request.UpdatePostRequest;
+import com.metrica.vibely.model.request.UpdateSavedByPostRequest;
+import com.metrica.vibely.model.response.create.CreatePostResponse;
+import com.metrica.vibely.model.response.get.GetPostResponse;
+import com.metrica.vibely.model.response.update.UpdateLikedByPostResponse;
+import com.metrica.vibely.model.response.update.UpdatePostResponse;
+import com.metrica.vibely.model.response.update.UpdateSavedByPostResponse;
+import com.metrica.vibely.service.PostService;
+
+import jakarta.validation.Valid;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <h1>Post Controller</h1>
+ * 
+ * @since 2023-11-27
+ * @version 1.0
+ * @author Adri
+ */
+@RestController
+@RequestMapping("/api/v1/posts")
 public class PostController {
 
-}
+    // <<-FIELDS->>
+	private ResponseManager responseManager;
+    private PostService postService;
+
+    // <<-CONSTRUCTOR->>
+    @Autowired
+    public PostController(final PostService postService, final ResponseManager responseManager) {
+    	this.responseManager = responseManager;
+        this.postService = postService;
+    }
+
+    // <<-METHODS->>
+    @GetMapping("/{id}")
+    public ResponseEntity<GetPostResponse> getById(@PathVariable UUID id) {
+        PostDTO postDTO = this.postService.getById(id);
+        return this.responseManager.generateGetResponse(postDTO);
+    }
+	
+	@PostMapping("/create")
+    public ResponseEntity<CreatePostResponse> create(
+            @RequestBody
+            @Valid
+            CreatePostRequest createPostRequest,
+            BindingResult bindingResult
+    ) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
+        PostDTO createdDTO = this.postService.create(createPostRequest.toPostDTO());
+        return this.responseManager.generateCreateResponse(createdDTO);
+    }

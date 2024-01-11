90afa03c914210e59ec348cdaa9f6a8efc12c599
gonzalorg879 <gonzalorg8799@gmail.com>
2024-01-11 12:19:26 +0100

    hotfixes in dev

--- data/src/main/java/com/metrica/vibely/data/repository/PostRepository.java
+++ data/src/main/java/com/metrica/vibely/data/repository/PostRepository.java
@@ -1,11 +1,30 @@
 package com.metrica.vibely.data.repository;
 
+import java.util.Optional;
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.metrica.vibely.data.entity.Post;
+import com.metrica.vibely.data.entity.User;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, java.util.UUID>{
 
+    /**
+     * @params username, content
+     * @return Post
+     * @throws NoSuchElementException
+     */
+//	Optional<Post> findByContentandUser(String username, String content);
+
+    /**
+     * @params following
+     * @return TreeSet Posts
+     * @throws NoSuchElementException
+     */
+//	Set<Post> getFollowingPost(Set<User> following);
+
 }
+

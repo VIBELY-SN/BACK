ce9aab68dce72097b325e6520005170e9e412416
Raul <Raul@DESKTOP-T5U920N>
2024-01-11 12:01:00 +0100

    methods getters and setters

--- application/src/main/java/com/metrica/vibely/model/response/get/BasicInfoResponse.java
+++ application/src/main/java/com/metrica/vibely/model/response/get/BasicInfoResponse.java
@@ -23,4 +23,91 @@ public class BasicInfoResponse {
     public BasicInfoResponse() {
     }
 	
+ // <<-METHOD->>
+    public <T extends UserDTO> BasicInfoResponse generateResponse(T dto) {
+        this.setUserId   (dto.getUserId());
+        this.setUsername (dto.getUsername());
+        this.setNickname (dto.getNickname());
+        this.setEmail    (dto.getEmail());
+        this.setState    (dto.getState());
+        this.setPrivacy  (dto.getPrivacy());
+        this.setFollowers(dto.getFollowers());
+        this.setFollowing(dto.getFollowing());
+        this.setPosts    (dto.getPosts());
+        
+        return this;
+    }
+    
+    // <<-GETTERS & SETTERS->>
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public UserState getState() {
+        return state;
+    }
+
+    public void setState(UserState state) {
+        this.state = state;
+    }
+
+    public PrivacyType getPrivacy() {
+        return privacy;
+    }
+
+    public void setPrivacy(PrivacyType privacy) {
+        this.privacy = privacy;
+    }
+
+    public Set<UUID> getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(Set<UUID> followers) {
+        this.followers = followers;
+    }
+
+    public Set<UUID> getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(Set<UUID> following) {
+        this.following = following;
+    }
+
+    public Set<UUID> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(Set<UUID> posts) {
+        this.posts = posts;
+    }
 }

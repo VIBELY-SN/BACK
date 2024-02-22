e4b5de848f5b3869166aaa29270d2bd4749e27e6
Óscar Águeda Peña <120451816+oagueda@users.noreply.github.com>
2024-02-22 17:28:44 +0100

    Update CorsConfiguration.java

--- application/src/main/java/com/metrica/vibely/conf/CorsConfiguration.java
+++ application/src/main/java/com/metrica/vibely/conf/CorsConfiguration.java
@@ -15,7 +15,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://vibely-api.oagueda.xyz", "https://vibely.oagueda.xyz","http://localhost:4200","vibely-api.oagueda.xyz")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("x-api-key")
